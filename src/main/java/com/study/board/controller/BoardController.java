1c0cd59488bdccbf4aad622e3bc5329355836f95
JIHYEISWISDOM <wlgp8958@naver.com>
2022-10-07 16:28:22 +0900

    Initial commit

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,27 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board/write")
+    public String boardWriteForm() {
+        return "boardWrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        System.out.println(board.getTitle());
+
+
+        return "";
+    }
+
+
+}
